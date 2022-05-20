258d552053e79a72288298d62103067413c7c006
Starlin de Jesus Medina <starlinjesus612@gmail.com>
2022-05-20 16:15:14 -0400

    completed formulario generico

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@ import {  RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="bg-primary shadow p-2 mb-4">
-    <h1 class="text-white text-center fs-3">Psicopata amazon</h1>
-  </header>
   <RouterView />
 </template>
 
